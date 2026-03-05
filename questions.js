23ae4a4f7a9969c6ad68f384ed142e3caed355e4
Shabaka Malik Banks <13648045+zerobleek@users.noreply.github.com>
2026-03-05 06:40:26 -0500

    Add movie trivia questions to questions.js
    Added a comprehensive set of movie trivia questions categorized by difficulty levels: easy, medium, hard, insane, and kevin.

---
+++ questions.js
@@ -0,0 +1,268 @@
+const questions = {
+
+easy: [
+{
+question:"Which movie features the quote 'I'll be back'?",
+choices:["The Terminator","RoboCop","Predator","Total Recall"],
+correct:0
+},
+{
+question:"Which movie features a park of cloned dinosaurs?",
+choices:["Jurassic Park","King Kong","The Lost World","Planet of the Apes"],
+correct:0
+},
+{
+question:"Who played Jack in Titanic?",
+choices:["Leonardo DiCaprio","Brad Pitt","Matt Damon","Johnny Depp"],
+correct:0
+},
+{
+question:"Which movie features a time traveling DeLorean?",
+choices:["Back to the Future","Looper","Tenet","Primer"],
+correct:0
+},
+{
+question:"Which superhero is Bruce Wayne?",
+choices:["Batman","Superman","Spider-Man","Iron Man"],
+correct:0
+},
+{
+question:"Which animated movie features a clownfish named Nemo?",
+choices:["Finding Nemo","Shark Tale","The Reef","Moana"],
+correct:0
+},
+{
+question:"Who directed Jurassic Park?",
+choices:["Steven Spielberg","James Cameron","George Lucas","Ridley Scott"],
+correct:0
+},
+{
+question:"Which actor plays Iron Man?",
+choices:["Robert Downey Jr.","Chris Evans","Mark Ruffalo","Chris Hemsworth"],
+correct:0
+},
+{
+question:"Which wizard school appears in Harry Potter?",
+choices:["Hogwarts","Durmstrang","Beauxbatons","Ilvermorny"],
+correct:0
+},
+{
+question:"Which movie features a giant shark attacking a beach town?",
+choices:["Jaws","Deep Blue Sea","The Meg","Open Water"],
+correct:0
+}
+],
+
+medium:[
+{
+question:"What is the name of the shark hunter in Jaws?",
+choices:["Quint","Brody","Hooper","Lucas"],
+correct:0
+},
+{
+question:"Who directed Pulp Fiction?",
+choices:["Quentin Tarantino","Martin Scorsese","Guy Ritchie","Danny Boyle"],
+correct:0
+},
+{
+question:"Who played Neo in The Matrix?",
+choices:["Keanu Reeves","Laurence Fishburne","Hugo Weaving","Carrie-Anne Moss"],
+correct:0
+},
+{
+question:"Which movie features the quote 'I'm the king of the world'?",
+choices:["Titanic","Cast Away","Apollo 13","The Perfect Storm"],
+correct:0
+},
+{
+question:"Which actor played the Joker in The Dark Knight?",
+choices:["Heath Ledger","Joaquin Phoenix","Jack Nicholson","Jared Leto"],
+correct:0
+},
+{
+question:"What is Indiana Jones’ profession?",
+choices:["Archaeologist","Historian","Explorer","Detective"],
+correct:0
+},
+{
+question:"Who directed Avatar?",
+choices:["James Cameron","Ridley Scott","Peter Jackson","Denis Villeneuve"],
+correct:0
+},
+{
+question:"Which movie features HAL 9000?",
+choices:["2001: A Space Odyssey","Alien","Blade Runner","Moon"],
+correct:0
+},
+{
+question:"What is the name of the hobbit played by Elijah Wood?",
+choices:["Frodo","Bilbo","Samwise","Pippin"],
+correct:0
+},
+{
+question:"Which movie features the quote 'Say hello to my little friend'?",
+choices:["Scarface","Casino","Heat","The Untouchables"],
+correct:0
+}
+],
+
+hard:[
+{
+question:"What was the name of the boat in Jaws?",
+choices:["The Orca","Sea Spirit","Poseidon","Blue Water"],
+correct:0
+},
+{
+question:"Who directed Blade Runner?",
+choices:["Ridley Scott","James Cameron","Tony Scott","Paul Verhoeven"],
+correct:0
+},
+{
+question:"Which movie features Travis Bickle?",
+choices:["Taxi Driver","Drive","Heat","Nightcrawler"],
+correct:0
+},
+{
+question:"Who directed Fight Club?",
+choices:["David Fincher","Christopher Nolan","Danny Boyle","Guy Ritchie"],
+correct:0
+},
+{
+question:"Which island is Jurassic Park located on?",
+choices:["Isla Nublar","Isla Sorna","Skull Island","Isla Verde"],
+correct:0
+},
+{
+question:"Which movie features Anton Chigurh?",
+choices:["No Country for Old Men","Sicario","Prisoners","Hell or High Water"],
+correct:0
+},
+{
+question:"Who directed Goodfellas?",
+choices:["Martin Scorsese","Francis Ford Coppola","Brian De Palma","Michael Mann"],
+correct:0
+},
+{
+question:"Who directed The Shining?",
+choices:["Stanley Kubrick","John Carpenter","Ridley Scott","Roman Polanski"],
+correct:0
+},
+{
+question:"Which movie features the Nostromo spaceship?",
+choices:["Alien","Sunshine","Event Horizon","Moon"],
+correct:0
+},
+{
+question:"Who directed The Grand Budapest Hotel?",
+choices:["Wes Anderson","Noah Baumbach","Greta Gerwig","Richard Linklater"],
+correct:0
+}
+],
+
+insane:[
+{
+question:"Who directed Seven Samurai?",
+choices:["Akira Kurosawa","Kenji Mizoguchi","Yasujiro Ozu","Masaki Kobayashi"],
+correct:0
+},
+{
+question:"Which actor played the original Mad Max?",
+choices:["Mel Gibson","Russell Crowe","Hugh Jackman","Eric Bana"],
+correct:0
+},
+{
+question:"Who directed Eraserhead?",
+choices:["David Lynch","David Cronenberg","Stanley Kubrick","John Carpenter"],
+correct:0
+},
+{
+question:"Which movie features Alex DeLarge?",
+choices:["A Clockwork Orange","Brazil","Stalker","THX 1138"],
+correct:0
+},
+{
+question:"Who directed The Seventh Seal?",
+choices:["Ingmar Bergman","Federico Fellini","Michelangelo Antonioni","Andrei Tarkovsky"],
+correct:0
+},
+{
+question:"Which film features Roy Batty?",
+choices:["Blade Runner","Dark City","Total Recall","Dune"],
+correct:0
+},
+{
+question:"Who directed Apocalypse Now?",
+choices:["Francis Ford Coppola","Oliver Stone","Martin Scorsese","Stanley Kubrick"],
+correct:0
+},
+{
+question:"Which movie features Daniel Plainview?",
+choices:["There Will Be Blood","Magnolia","The Master","Phantom Thread"],
+correct:0
+},
+{
+question:"Which movie contains the quote 'Here's looking at you kid'?",
+choices:["Casablanca","Citizen Kane","The Maltese Falcon","Notorious"],
+correct:0
+},
+{
+question:"Who directed Oldboy (2003)?",
+choices:["Park Chan-wook","Bong Joon-ho","Kim Jee-woon","Na Hong-jin"],
+correct:0
+}
+],
+
+kevin:[
+{
+question:"In Blade Runner which corporation makes replicants?",
+choices:["Tyrell Corporation","Cyberdyne Systems","OmniCorp","Weyland Corp"],
+correct:0
+},
+{
+question:"Who composed the score for The Good, the Bad and the Ugly?",
+choices:["Ennio Morricone","John Williams","Hans Zimmer","Nino Rota"],
+correct:0
+},
+{
+question:"Who directed Stalker?",
+choices:["Andrei Tarkovsky","Sergei Eisenstein","Nikita Mikhalkov","Elem Klimov"],
+correct:0
+},
+{
+question:"Which film features Nurse Ratched?",
+choices:["One Flew Over the Cuckoo's Nest","Misery","Psycho","The Shining"],
+correct:0
+},
+{
+question:"Who directed Mulholland Drive?",
+choices:["David Lynch","Paul Thomas Anderson","David Fincher","Coen Brothers"],
+correct:0
+},
+{
+question:"Which director made Ran?",
+choices:["Akira Kurosawa","Hayao Miyazaki","Nagisa Oshima","Takeshi Kitano"],
+correct:0
+},
+{
+question:"Which film features Harry Lime?",
+choices:["The Third Man","The Maltese Falcon","Touch of Evil","Double Indemnity"],
+correct:0
+},
+{
+question:"Who directed The Lighthouse?",
+choices:["Robert Eggers","Ari Aster","Jordan Peele","Alex Garland"],
+correct:0
+},
+{
+question:"Which director made Parasite?",
+choices:["Bong Joon-ho","Park Chan-wook","Kim Ki-duk","Lee Chang-dong"],
+correct:0
+},
+{
+question:"Which film features the character Travis Henderson?",
+choices:["Paris Texas","Badlands","Days of Heaven","Midnight Cowboy"],
+correct:0
+}
+]
+
+};
