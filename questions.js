5b5c801230bbd302fcef721080d241c70c977e88
Shabaka Malik Banks <13648045+zerobleek@users.noreply.github.com>
2026-03-12 06:36:34 -0400

    Refactor question generation and structure

--- questions.js
+++ questions.js
@@ -1,50 +1,173 @@
-function generateQuestions(text, answers, correct){
-
-let arr=[]
-
-for(let i=1;i<=40;i++){
-
-arr.push({
-q:text + " #" + i,
-a:answers,
-c:correct
-})
-
-}
-
-return arr
-}
-
 const QUESTIONS = {
 
-easy: generateQuestions(
-"Who directed Titanic?",
-["James Cameron","Steven Spielberg","Christopher Nolan","Ridley Scott"],
-0
-),
+easy: [
 
-medium: generateQuestions(
-"What year was Jurassic Park released?",
-["1990","1993","1997","2001"],
-1
-),
+{
+q: "Who directed Titanic?",
+a: ["James Cameron","Steven Spielberg","Ridley Scott","Christopher Nolan"],
+c: 0
+},
 
-hard: generateQuestions(
-"Who composed the Star Wars score?",
-["Hans Zimmer","John Williams","Danny Elfman","Howard Shore"],
-1
-),
+{
+q: "Which movie features a shark terrorizing a beach town?",
+a: ["Jaws","Deep Blue Sea","The Meg","Sharknado"],
+c: 0
+},
 
-insane: generateQuestions(
-"In which film does Travis Bickle appear?",
-["Taxi Driver","Scarface","Heat","The Godfather"],
-0
-),
+{
+q: "What is the name of the toy cowboy in Toy Story?",
+a: ["Buzz","Woody","Jessie","Andy"],
+c: 1
+},
 
-kevin: generateQuestions(
-"What movie features an exploding building?",
-["Die Hard","Speed","Fight Club","All of them"],
-3
-)
+{
+q: "Who played Jack in Titanic?",
+a: ["Brad Pitt","Leonardo DiCaprio","Matt Damon","Johnny Depp"],
+c: 1
+},
+
+{
+q: "Which film features dinosaurs cloned from DNA?",
+a: ["Jurassic Park","King Kong","Godzilla","Avatar"],
+c: 0
+},
+
+{
+q: "Which movie says 'May the Force be with you'?",
+a: ["Star Wars","Star Trek","Dune","Avatar"],
+c: 0
+},
+
+{
+q: "Who played Iron Man?",
+a: ["Chris Evans","Robert Downey Jr","Chris Hemsworth","Mark Ruffalo"],
+c: 1
+},
+
+{
+q: "Which movie features a giant iceberg collision?",
+a: ["Titanic","Poseidon","The Perfect Storm","Waterworld"],
+c: 0
+},
+
+{
+q: "What movie has a talking donkey?",
+a: ["Shrek","Frozen","Toy Story","Moana"],
+c: 0
+},
+
+{
+q: "Which movie features a theme park with dinosaurs?",
+a: ["Jurassic Park","Avatar","King Kong","Godzilla"],
+c: 0
+},
+
+{
+q: "Which film features the character Forrest Gump?",
+a: ["Forrest Gump","Cast Away","The Terminal","Saving Private Ryan"],
+c: 0
+},
+
+{
+q: "Who played the Joker in The Dark Knight?",
+a: ["Heath Ledger","Joaquin Phoenix","Jack Nicholson","Jared Leto"],
+c: 0
+},
+
+{
+q: "What movie features blue alien people on Pandora?",
+a: ["Avatar","Alien","Prometheus","Guardians of the Galaxy"],
+c: 0
+},
+
+{
+q: "Which movie has a DeLorean time machine?",
+a: ["Back to the Future","Looper","Tenet","Edge of Tomorrow"],
+c: 0
+},
+
+{
+q: "Who directed Avatar?",
+a: ["James Cameron","Christopher Nolan","Ridley Scott","Denis Villeneuve"],
+c: 0
+},
+
+{
+q: "Which movie features a boxer named Rocky?",
+a: ["Rocky","Creed","Raging Bull","Million Dollar Baby"],
+c: 0
+},
+
+{
+q: "Which movie features a wizard named Harry?",
+a: ["Harry Potter","Lord of the Rings","Narnia","Fantastic Beasts"],
+c: 0
+},
+
+{
+q: "What movie features Elsa and Anna?",
+a: ["Frozen","Tangled","Brave","Encanto"],
+c: 0
+},
+
+{
+q: "Which movie features talking cars?",
+a: ["Cars","Transformers","Speed Racer","Mad Max"],
+c: 0
+},
+
+{
+q: "What movie features a clownfish searching for his son?",
+a: ["Finding Nemo","Shark Tale","The Little Mermaid","Moana"],
+c: 0
+},
+
+{
+q: "Which movie features superheroes called Avengers?",
+a: ["The Avengers","Justice League","Fantastic Four","Watchmen"],
+c: 0
+},
+
+{
+q: "Which movie has a giant ape climbing a skyscraper?",
+a: ["King Kong","Rampage","Godzilla","Pacific Rim"],
+c: 0
+},
+
+{
+q: "Which movie features a robot named WALL-E?",
+a: ["WALL-E","Big Hero 6","Transformers","Short Circuit"],
+c: 0
+},
+
+{
+q: "Which movie features pirates searching for treasure?",
+a: ["Pirates of the Caribbean","Treasure Planet","Hook","Peter Pan"],
+c: 0
+},
+
+{
+q: "Which movie features a ring that must be destroyed?",
+a: ["Lord of the Rings","Harry Potter","Narnia","Dune"],
+c: 0
+}
+
+],
+
+medium: [
+/* paste your 25 medium questions here in the same format */
+],
+
+hard: [
+/* paste your 25 hard questions here */
+],
+
+insane: [
+/* paste your 25 insane questions here */
+],
+
+kevin: [
+/* paste Kevin mode questions here */
+]
 
 }
